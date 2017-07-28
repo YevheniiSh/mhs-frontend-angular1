6afe26d6eb7f06c25f1aaa0b9e9155243736ca45
yevheniish <yevheniishc@softwareplanet.uk.com>
2017-07-28 12:39:03 +0300

    added game-type

--- app/admin/game-build/game-type/GameTypeController.js
+++ app/admin/game-build/game-type/GameTypeController.js
@@ -2,7 +2,18 @@ angular
     .module('mhs.admin')
     .controller('GameTypeController', GameTypeController);
 
-function GameTypeController($scope) {
+function GameTypeController() {
     let vm = this;
-    vm.title = 'test';
+
+    let rounds = [];
+    let quizSequenceNumber = 1;
+
+    vm.changeRoundCount = function (count) {
+        for (let i = 0; i < count; i++) {
+            let quiz = {sequenceNumber: quizSequenceNumber++, quizzess: 10};
+            rounds.push(quiz);
+        }
+    };
+
+    vm.rounds = rounds;
 }
