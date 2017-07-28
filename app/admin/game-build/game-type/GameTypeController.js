6d56e1ab9f16b5e35e05feb4438dfa757a687ecb
ihorl <ihorlu@softwareplanet.uk.com>
2017-07-28 14:37:01 +0300

    remove game-type  controller

--- app/admin/game-build/game-type/GameTypeController.js
+++ app/admin/game-build/game-type/GameTypeController.js
@@ -1,21 +1,21 @@
-angular
-    .module('mhs.admin')
-    .controller('GameTypeController', GameTypeController);
-
-function GameTypeController() {
-    let vm = this;
-
-    let rounds = [];
-    let quizSequenceNumber = 1;
-
-    vm.changeRoundCount = function (count) {
-        quizSequenceNumber = 1;
-        rounds.splice(0, rounds.length);
-        for (let i = 0; i < count; i++) {
-            let quiz = {sequenceNumber: quizSequenceNumber++, quizzess: 10};
-            rounds.push(quiz);
-        }
-    };
-
-    vm.rounds = rounds;
-}
+// angular
+//     .module('mhs.admin')
+//     .controller('GameTypeController', GameTypeController);
+//
+// function GameTypeController() {
+//     let vm = this;
+//
+//     let rounds = [];
+//     let quizSequenceNumber = 1;
+//
+//     vm.changeRoundCount = function (count) {
+//         quizSequenceNumber = 1;
+//         rounds.splice(0, rounds.length);
+//         for (let i = 0; i < count; i++) {
+//             let quiz = {sequenceNumber: quizSequenceNumber++, quizzess: 10};
+//             rounds.push(quiz);
+//         }
+//     };
+//
+//     vm.rounds = rounds;
+// }
--- app/admin/game-build/game-type/game-type-component.js
+++ app/admin/game-build/game-type/game-type-component.js
@@ -1,7 +1,27 @@
 angular
     .module('mhs.admin')
     .component('gameType', {
-        controller: 'GameTypeController',
+        controller() {
+            let vm = this;
+            createGameType(vm);
+        },
         templateUrl: 'admin/game-build/game-type/game-type.html',
         css: 'admin/game-build/game-type/game-type.css'
     });
+
+function createGameType(vm) {
+
+    let rounds = [];
+    let quizSequenceNumber = 1;
+
+    vm.changeRoundCount = function (count) {
+        quizSequenceNumber = 1;
+        rounds.splice(0, rounds.length);
+        for (let i = 0; i < count; i++) {
+            let quiz = {sequenceNumber: quizSequenceNumber++, quizzess: 10};
+            rounds.push(quiz);
+        }
+    };
+
+    vm.rounds = rounds;
+}
