6d483134fecb8dc649927d15ef809cbc808906f9
OleksiiLy <oleksiily@softwareplanetgroup.com>
2017-07-31 14:53:51 +0300

    result-setup-page

--- app/admin/result-setup/resultSetupService.js
+++ app/admin/result-setup/resultSetupService.js
@@ -1,23 +1,28 @@
 'use strict';
 angular.module('mhs.admin').factory('resultSetupService', function ($q) {
-    let gameService = new GameService(DbConnection.getConnection());
-    return {
-        getData: function () {
-            let defer = $q.defer();
-            gameService.getGameById('-KqClHrHpGmoZRcKfOZI')
-                .then((game) => {
-                    defer.resolve(game.val());
-                });
-            return defer.promise;
-        },
-        setData: function (roundNumber, quizNumber, teamId, score) {
-            let defer = $q.defer();
-            let result = new Result(roundNumber, quizNumber, teamId);
-            result.setScore(score)
-            resultService.saveResult(result, '-KqClHrHpGmoZRcKfOZI').then((resultKey) => {
-                defer.resolve(resultKey);
-            });
-            return defer;
-        }
+    var gameService = new GameService(DbConnection.getConnection());
+    var service = {
+        getData: getData,
+        setData: setData
     }
+
+    function getData() {
+        let defer = $q.defer();
+        gameService.getGameById('-KqClHrHpGmoZRcKfOZI')
+            .then((game) => {
+                defer.resolve(game.val());
+            });
+        return defer.promise;
+    }
+    function setData(result) {
+        let defer = $q.defer();
+        let result = new Result(roundNumber, quizNumber, teamId);
+        result.setScore(score)
+        resultService.saveResult(result, '-KqClHrHpGmoZRcKfOZI').then((resultKey) => {
+            defer.resolve(resultKey);
+        });
+        return defer.promise;
+    }
+
+    return service;
 });
