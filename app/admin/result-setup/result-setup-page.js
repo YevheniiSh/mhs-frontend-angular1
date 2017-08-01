7cb642ce0a6a68d9acf92f5b7b07ec41a786569e
OleksiiLy <oleksiily@softwareplanetgroup.com>
2017-08-01 15:20:13 +0300

    result-setup-page

--- app/admin/result-setup/result-setup-page.js
+++ app/admin/result-setup/result-setup-page.js
@@ -2,22 +2,24 @@
 angular.module('resultSetup')
     .component('resultSetup', {
         templateUrl: 'admin/result-setup/result-setup-page.html',
-        controller: function ResultSetupController(resultSetupService, $routeParams) {
+        controller: function ResultSetupController(resultSetupService,$routeParams,$location,$scope) {
             let vm = this;
             let gameId = $routeParams.gameId;
+            vm.quizNumber = $routeParams.quizNumber;
             vm.currentRound = $routeParams.roundNumber;
             resultSetupService.getData(gameId)
                 .then((game) => {
                     vm.quizzes = [];
                     vm.teams = game.teams;
                     let quizCount = game.rounds[$routeParams.roundNumber];
+                    console.log(quizCount);
                     for (let i = 1; i <= quizCount; i++) {
                         vm.quizzes.push({number: i, answered: false});
                     }
-                    if ($routeParams.quizNumber > vm.quizCount){
-                        vm.quizNumber = 1;
+                    if ($routeParams.quizNumber > parseInt(quizCount)){
+                        vm.setQuiz(1);
                     }else{
-                        vm.quizNumber = $routeParams.quizNumber;
+                        vm.setQuiz($routeParams.quizNumber);
                     }
                     vm.teamsScore = [];
                 });
@@ -34,22 +36,24 @@ angular.module('resultSetup')
             };
             vm.setResult = function () {
                 let results = [];
+                let promices = [];
                 angular.forEach(vm.teams, function (team, key) {
                     results.push(new Result(vm.currentRound, vm.quizNumber, key));
                 });
-                angular.forEach(results, function (result, key) {
-                    if (vm.teamsScore[key]) {
-                        resultSetupService.setQuizResult(result, vm.teamsScore[key]);
-                    }
-                    else {
-                        resultSetupService.setQuizResult(result, 0);
-                    }
-                });
                 vm.quizzes[vm.quizNumber - 1].answered = true;
-                if (vm.quizNumber  !== vm.quizzes.length) {
-                    vm.quizNumber++;
-                    vm.setQuiz(vm.quizNumber);
-                }
+                angular.forEach(results, function (result,key) {
+                    promices.push(resultSetupService.setQuizResult(result,vm.teamsScore[key]));
+                });
+                Promise.all(promices)
+                    .then(()=>{
+                        if (vm.quizNumber  < vm.quizzes.length) {
+                            vm.quizNumber++;
+                            vm.setQuiz(vm.quizNumber);
+                        }else {
+                            $location.path('/round-status/' + gameId);
+                        }
+                    })
+                    .then($scope.$apply);
             };
         }
     });
