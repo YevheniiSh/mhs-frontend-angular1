408e1af5e52faf7aed05bda11bf6cd0a78ca0d9a
ihorl <ihorlu@softwareplanet.uk.com>
2017-08-01 09:46:54 +0300

    added redirect to round-status

--- app/admin/game-build/game-type/game-type.component.js
+++ app/admin/game-build/game-type/game-type.component.js
@@ -2,53 +2,49 @@
 angular.module('gameType')
     .component('gameType', {
             templateUrl: 'admin/game-build/game-type/game-type.html',
-            controller: ['GameServiceFactory', '$routeParams', function (GameService, $routeParams) {
+            controller: ['GameServiceFactory', '$routeParams', '$location', '$rootScope',
+                function (GameService, $routeParams, $location, $rootScope) {
 
-                let vm = this;
-                let rounds = [];
-                let quizSequenceNumber = 1;
+                    let vm = this;
+                    let rounds = [];
+                    let quizSequenceNumber = 1;
 
-                vm.changeRoundCount = function (count) {
-                    quizSequenceNumber = 1;
-                    rounds.splice(0, rounds.length);
-                    for (let i = 0; i < count; i++) {
-                        let quiz = {sequenceNumber: quizSequenceNumber++, quizzess: 10};
-                        rounds.push(quiz);
-                    }
+                    vm.changeRoundCount = function (count) {
+                        quizSequenceNumber = 1;
+                        rounds.splice(0, rounds.length);
+                        for (let i = 0; i < count; i++) {
+                            let quiz = {sequenceNumber: quizSequenceNumber++, quizzess: 10};
+                            rounds.push(quiz);
+                        }
 
-                };
+                    };
 
-                vm.rounds = rounds;
+                    vm.rounds = rounds;
 
-                vm.buildGame = function () {
-
-                    GameService
-                        .getGameById($routeParams.gameId)
-                        .then((res) => {
-
-                            console.log(res.val());
-                            // res.val().rounds = [];
-                            //  game.rounds = [];
-                            //  console.dir("game" + res.val());
-
-                            let game = GameService.convertFromFirebase(res.val());
-                            console.dir("game  1 " + game   );
-
-                            let gameBuilder = new GameBuilder(game);
-                            let roundArray = [];
-
-                            for (let i = 0; i <rounds.length; i++) {
+                    vm.buildGame = function () {
+                        let gameId = $routeParams.gameId;
+                        GameService
+                            .getGameById(gameId)
+                            .then((res) => {
+                                let game = GameService.convertFromFirebase(res.val());
 
 
-                                roundArray.push(rounds[i].quizzess);
-                            }
-                            gameBuilder.addRoundsArray(roundArray);
-                            console.dir("game builder 2 " + gameBuilder);
+                                let gameBuilder = new GameBuilder(game);
+                                let roundArray = [];
 
-                            GameService.save(gameBuilder.game, $routeParams.gameId);
-                        });
-                };
+                                for (let i = 0; i < rounds.length; i++) {
 
-            }]
+                                    roundArray.push(rounds[i].quizzess);
+                                }
+                                gameBuilder.addRoundsArray(roundArray);
+
+                                GameService.save(gameBuilder.game, gameId);
+
+                                $location.path('/round-status/' + gameId);
+                                $rootScope.$apply();
+                            });
+                    };
+
+                }]
         }
     );
