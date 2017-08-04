bd6daaf9f2d3f24f88e7d09144cf476e5f5930d5
ihorl <ihorlu@softwareplanet.uk.com>
2017-08-04 16:52:18 +0300

    added date convert to game

--- app/public/js/GameBuilder.js
+++ app/public/js/GameBuilder.js
@@ -41,7 +41,7 @@ class GameBuilder {
     }
 
     addDate(date) {
-        this.game.date = date;
+        this.game.date = convertDate(date);
     }
 
     addLocation(location) {
@@ -63,4 +63,17 @@ class GameBuilder {
     buildGame() {
         return this.game;
     }
+}
+function convertDate(date) {
+    let dd = date.getDate();
+    let mm = date.getMonth()+1; //January is 0!
+
+    let yyyy = date.getFullYear();
+    if(dd<10){
+        dd='0'+dd;
+    }
+    if(mm<10){
+        mm='0'+mm;
+    }
+    return dd+'/'+mm+'/'+yyyy;
 }
