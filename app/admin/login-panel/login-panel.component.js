63e01c9358b1684357a92a6597241bdb6f42fc48
yevheniish <yevheniishc@softwareplanet.uk.com>
2017-08-07 12:09:23 +0300

    login panel fix

--- app/admin/login-panel/login-panel.component.js
+++ app/admin/login-panel/login-panel.component.js
@@ -1,12 +1,15 @@
 angular.module('login-panel')
     .component('loginPanel', {
         templateUrl: "admin/login-panel/login-panel.html",
-        controller: ['userAuthService', '$location', LoginPanel]
+        controller: ['userAuthService', '$location', '$rootScope', LoginPanel]
     });
 
-function LoginPanel(userAuthService, $location) {
+function LoginPanel(userAuthService, $location, $rootScope) {
     this.logOut = function () {
-        userAuthService.signOut();
-        $location.path('/#!/login');
+        userAuthService.signOut()
+            .then(() => {
+                $rootScope.currentUser = null;
+                $location.path('/#!/login');
+            });
     }
 }
