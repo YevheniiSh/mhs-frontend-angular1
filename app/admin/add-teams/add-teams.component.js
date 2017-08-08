19b4e03b7cef1584459f47f2cb670aac94937ef1
bohdanh <bohdanhu@softwareplanet.uk.com>
2017-08-08 13:41:14 +0300

    Save teams update

--- app/admin/add-teams/add-teams.component.js
+++ app/admin/add-teams/add-teams.component.js
@@ -38,51 +38,52 @@ angular.module('addTeams')
                 //     });
                 //     this.selected = null;
                 // };
-
-                this.newTeam = function () {
-                    this.teams.push({name: this.newTeamName});
-                    this.newTeamName = '';
-                };
+                //
+                // this.newTeam = function () {
+                //     this.teams.push({name: this.newTeamName});
+                //     this.newTeamName = '';
+                // };
 
                 this.onClick = function (item) {
                     item.selected = !item.selected;
                     console.log(item);
                 };
 
-                this.deleteTeam = function (index) {
-                    if (this.teams.length > 2) {
-                        let removedItem = this.teams.splice(index, 1)[0];
-                        console.log(removedItem);
-                        if (removedItem.teamId !== undefined) {
-                            this.teamsFromDB.push({teamId: removedItem.teamId, name: removedItem.name});
-                        }
-                    }
-                    else {
-                        alert('min number of teams is 2');
-                    }
-                };
+                // this.deleteTeam = function (index) {
+                //     if (this.teams.length > 2) {
+                //         let removedItem = this.teams.splice(index, 1)[0];
+                //         console.log(removedItem);
+                //         if (removedItem.teamId !== undefined) {
+                //             this.teamsFromDB.push({teamId: removedItem.teamId, name: removedItem.name});
+                //         }
+                //     }
+                //     else {
+                //         alert('min number of teams is 2');
+                //     }
+                // };
 
-                this.removeDuplicates = function (originalArray, prop) {
-                    var newArray = [];
-                    var lookupObject = {};
-
-                    for (var i in originalArray) {
-                        lookupObject[originalArray[i][prop]] = originalArray[i];
-                    }
-
-                    for (i in lookupObject) {
-                        newArray.push(lookupObject[i]);
-                    }
-                    return newArray;
-                };
+                // this.removeDuplicates = function (originalArray, prop) {
+                //     var newArray = [];
+                //     var lookupObject = {};
+                //
+                //     for (var i in originalArray) {
+                //         lookupObject[originalArray[i][prop]] = originalArray[i];
+                //     }
+                //
+                //     for (i in lookupObject) {
+                //         newArray.push(lookupObject[i]);
+                //     }
+                //     return newArray;
+                // };
 
                 this.saveTeams = function () {
 
                     this.teamsFromDB.forEach((item) => {
-                        if (item.selected = true) {
+                        if (item.selected === true) {
                             this.teams.push({id: item.teamId, name: item.name});
                         }
                     });
+                    console.log(this.teams);
                     this.save();
                 };
 
