3881c8de62146ca3a31d1642f3c892cb34ea5708
Andrii Heiko <andriihe@softwareplanet.uk.com>
2017-08-11 15:48:39 +0300

    Add get request method

--- app/admin/add-teams/add-teams.component.js
+++ app/admin/add-teams/add-teams.component.js
@@ -2,91 +2,23 @@ angular.module('addTeams')
     .component('addTeams', {
         templateUrl: 'admin/add-teams/add-teams.html',
         css: 'admin/add-teams/add-teams.css',
-        controller: ['TeamServiceFactory',
-            'GameServiceFactory',
-            '$rootScope',
+        controller: ['teamRequestServiceFactory',
             '$location',
-
-            function (TeamService, GameService, $rootScope, $location) {
-
+            '$routeParams',
+            function (teamRequestService, $location,$routeParams) {
                 let vm = this;
-
-                this.selected = null;
-
-                this.teams = [];
-                this.teamsFromDB = [];
-
-                this.$onInit = onInit;
-
+                vm.gameId = $routeParams.gameId;
+                vm.$onInit = onInit;
                 function onInit() {
-                    vm.getTeams();
+                    vm.getRequests();
                 }
 
-                this.getTeams = function () {
-                    TeamService.getAllTeams().then((res) => {
-                        angular.forEach(res, (team) => {
-                            this.teamsFromDB.push({teamId: team.$id, name: team.name, selected: false});
-                        });
-                    });
-                };
-
-                this.onClick = function (item) {
-                    item.selected = !item.selected;
-                    console.log(item);
-                };
-
-                this.saveTeams = function () {
-
-                    this.teamsFromDB.forEach((item) => {
-                        if (item.selected === true) {
-                            this.teams.push({id: item.teamId, name: item.name});
-                        }
-                    });
-                    console.log(this.teams);
-                    this.save();
-                };
-
-                this.filter = {};
-
-                this.resetFilter = function () {
-                    this.filter = {};
-                };
-
-                this.setFilter = function (selected) {
-                    this.filter = {selected: selected};
-                };
-
-                this.addTeamToDb = function () {
-                    let teamBuilder = new TeamBuilder(TeamService, [{name: this.newTeamName}]);
-                    teamBuilder.setTeams()
-                        .then((res) => {
-                            console.log(res);
-                            res.forEach((item) => {
-                                this.teamsFromDB.unshift({teamId: item.id, name: item.name, selected: true});
-                            });
-                            this.newTeamName = '';
-                            return res;
+                vm.getRequests = function () {
+                    teamRequestService.getAllTeamRequestsByGameId(vm.gameId)
+                        .then((res)=>{
+                            vm.requests=res;
                         })
                 };
-
-                this.save = function () {
-                    let teamBuilder = new TeamBuilder(TeamService, this.teams);
-                    teamBuilder.setTeams()
-                        .then((res) => {
-                            return res;
-
-                        })
-                        .then((teams) => {
-                            let game = new GameBuilder().addTeams(teams).buildGame();
-                            return GameService.save(game);
-                        })
-                        .then((gameId) => {
-                            console.log(gameId);
-
-                            $location.path('/setup-game-type/' + gameId);
-                        });
-                }
-
             }]
 
     })
