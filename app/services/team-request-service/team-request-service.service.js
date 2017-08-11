d80d320c163a0b7e8e74ecf0bc84f086ac0f5e72
OleksiiLy <oleksiily@softwareplanetgroup.com>
2017-08-11 11:18:24 +0300

    team-request-service

--- app/services/team-request-service/team-request-service.service.js
+++ app/services/team-request-service/team-request-service.service.js
@@ -2,9 +2,58 @@ angular.module('teamRequestService')
     .factory('teamRequestServiceFactory', ['$firebaseArray', '$firebaseObject', 'firebaseDataService',
         function ($firebaseArray, $firebaseObject, firebaseDataService) {
 
+            let openGameRef = firebaseDataService.openGames;
 
-            function save(teamRequest) {
-                let obj = new $firebaseObject
+
+            return {
+                save: save,
+                getTeamRequest: getTeamRequest,
+                getAllTeamRequestsByGameId: getAllTeamRequestsByGameId
+            }
+
+            function save(gameId, teamRequest) {
+                let teamRequestRef = openGameRef.child(`${gameId}/requests`);
+                let teamRequestObj = new $firebaseObject(teamRequestRef.push());
+                teamRequestObj.$value = teamRequest;
+                return teamRequestObj.$save()
+                    .then(
+                        () => {
+                            return teamRequestObj.$id;
+                        }, (err) => {
+                            console.log(err);
+                            throw err;
+                        }
+                    )
+            }
+
+            function getTeamRequest(gameId, teamRequestId) {
+                let teamRequestRef = openGameRef.child(`${gameId}/requests/${teamRequestId}`);
+                return new $firebaseObject(teamRequestRef)
+                    .$loaded()
+                    .then(
+                        (res) => {
+                            return res;
+                        },
+                        (err) => {
+                            console.log(err);
+                            throw err;
+                        }
+                    )
+            }
+
+            function getAllTeamRequestsByGameId(gameId) {
+                let requestsRef = openGameRef.child(`${gameId}/requests`);
+                return new $firebaseArray(requestsRef)
+                    .$loaded()
+                    .then(
+                        (teamsRequests) => {
+                            return teamsRequests;
+                        },
+                        (err) => {
+                            console.log(err);
+                            throw err;
+                        }
+                    )
             }
         }]
     );
