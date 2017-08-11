1d5e92fef5194faaeabca759b7a454771086545e
OleksiiLy <oleksiily@softwareplanetgroup.com>
2017-08-11 09:51:18 +0300

    team-request-service

---
+++ app/services/team-request-service/team-request-service.service.js
@@ -0,0 +1,10 @@
+angular.module('teamRequestService')
+    .factory('teamRequestServiceFactory', ['$firebaseArray', '$firebaseObject', 'firebaseDataService',
+        function ($firebaseArray, $firebaseObject, firebaseDataService) {
+
+
+            function save(teamRequest) {
+                let obj = new $firebaseObject
+            }
+        }]
+    );
