baf47aa91e66aca922f7d51ea6c117cdf5e0cb59
yevheniish <yevheniishc@softwareplanet.uk.com>
2017-08-14 11:34:01 +0300

    fixed and refactored application-form package

--- app/player/application-form/application-form.component.js
+++ app/player/application-form/application-form.component.js
@@ -6,19 +6,23 @@
             controller: TeamRegister
         });
 
-    TeamRegister.$inject = ['TeamServiceFactory', 'GameServiceFactory', '$routeParams', '$window', '$location', 'teamRequestServiceFactory', 'OpenGameServiceFactory'];
+    TeamRegister.$inject = ['TeamServiceFactory', 'GameServiceFactory', '$routeParams', '$window', '$location', 'gameRequestServiceFactory', 'OpenGameServiceFactory'];
 
-    function TeamRegister(TeamService, GameService, $routeParams, $window, $location, TeamRequestService, OpenGameService) {
+    function TeamRegister(TeamService, GameService, $routeParams, $window, $location, gameRequestServiceFactory, OpenGameService) {
         let vm = this;
-        vm.teamName = "";
-        vm.fullName = "";
-        vm.phone = "";
-        vm.teamSize = 4;
-        vm.submitted = false;
+        vm.$onInit = onInit;
         let gameId = $routeParams.gameId;
 
+        function initRegisterForm() {
+            vm.teamName = "";
+            vm.fullName = "";
+            vm.phone = "";
+            vm.teamSize = 4;
+            vm.submitted = false;
+        }
+
         vm.saveRequest = function () {
-            TeamRequestService.save(gameId, {
+            gameRequestServiceFactory.save(gameId, {
                 teamName: vm.teamName,
                 fullName: vm.fullName,
                 phone: vm.phone,
@@ -27,7 +31,7 @@
                 teamId: "",         // TODO проверять есть ли такая команда
                 date: new Date().toDateString()
             })
-                .then((res) => {
+                .then(() => {
                     vm.submitted = true;
                     setTimeout(() => {
                         $window.history.back();
@@ -36,13 +40,26 @@
                 })
         };
 
+        vm.tets = function () {
+            TeamService.getAllTeams()
+                .then((res) => {
+                    let auto = res.filter((team) => {
+                        return team.name.startsWith(vm.teamName);
+                    });
+                    console.log(auto)
+                })
+        };
+
         vm.onBack = function () {
             $window.history.back();
         };
 
-        OpenGameService.getOpenGameById(gameId).then((res) => {
-                vm.gameDate = new Date(res.date).toLocaleDateString()
-            }
-        )
+        function onInit() {
+            initRegisterForm();
+            OpenGameService.getOpenGameById(gameId).then((res) => {
+                    vm.gameDate = new Date(res.date).toLocaleDateString()
+                }
+            )
+        }
     }
 })();
