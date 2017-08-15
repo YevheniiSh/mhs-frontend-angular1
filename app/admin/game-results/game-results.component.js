6dcca29be2c59741526cf94413dc18521ef004b5
AntonTry <antontr@interlink-ua.com>
2017-08-15 14:25:48 +0300

    Improve css attribute names in game results && add date to game results component

--- app/admin/game-results/game-results.component.js
+++ app/admin/game-results/game-results.component.js
@@ -1,18 +1,28 @@
 angular.module('gameResultsPage')
     .component('gameResults', {
         templateUrl: 'admin/game-results/game-results.html',
-        controller: ['ResultServiceFactory', '$rootScope', '$routeParams', '$location', function (ResultService, $rootScope, $routeParams, $location) {
+        controller: ['GameServiceFactory','ResultServiceFactory', '$rootScope', '$routeParams', '$location', function (GameService, ResultService, $rootScope, $routeParams, $location) {
             this.getDetails = function (teamResult) {
                 if (!$rootScope.presentationMode) {
                     console.log(teamResult.teamId);
                     $location.path(`/games/${$routeParams.gameId}/results/${teamResult.teamId}`);
                 }
             };
+            this.$onInit = onInit;
+            function onInit() {
+                this.gameId = $routeParams.gameId;
+
+                GameService.getGameStatus(this.gameId).then(status => {
+                    if (status === "current")
+                        GameService.getDate(status, this.gameId).then(v => this.date = new Date(v.$value).toLocaleDateString());
+                    if (status === "finished")
+                        GameService.getDate(status, this.gameId).then(v => this.date = new Date(v.$value).toLocaleDateString())
+                });
+            }
+
             ResultService.getParsedResults($routeParams.gameId)
                 .then((result) => {
                     this.results = result;
                 });
-
-
         }]
 });
