c701dcdbca9c422891cfacb2b039ce8fea90a6a5
AntonTry <antontr@interlink-ua.com>
2017-08-16 14:55:33 +0300

    Change game-result table styles && Fix bugs with tooltip in team results

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('mhs', [
     'ngRoute',
+    'ngMessages',
     'mhs.admin',
     'mhs.player',
     'mhs.version',
