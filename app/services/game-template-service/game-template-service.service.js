88b65592cdd290b7a7ec3325b8f9a47e6506d3ac
bohdanh <bohdanhu@softwareplanet.uk.com>
2017-08-17 16:19:29 +0300

    CRUD operations for gameTemplateService

--- app/services/game-template-service/game-template-service.service.js
+++ app/services/game-template-service/game-template-service.service.js
@@ -2,16 +2,38 @@ angular.module('gameTemplateService')
     .factory('gameTemplateServiceFactory', ['$firebaseArray', '$firebaseObject', 'firebaseDataService',
         function ($firebaseArray, $firebaseObject, firebaseDataService) {
 
-            let openGameRef = firebaseDataService.openGames;
+            let gameTemplatesRef = firebaseDataService.gameTemplates;
 
 
             return {
-                save: save
+                save: save,
+                getAll: getAll,
+                getById: getById,
+                remove: remove
             }
 
 
-            function save() {
+            function save(template) {
+                let fbObj = new $firebaseObject(gameTemplatesRef.push());
+                fbObj.$value = template;
+                fbObj.$save();
+                return fbObj.$loaded();
+            }
 
+            function getAll() {
+                let fbObj = new $firebaseArray(gameTemplatesRef);
+                return fbObj.$loaded();
+            }
+
+            function getById(templateId) {
+                let fbObj = new $firebaseObject(gameTemplatesRef.child(templateId));
+                return fbObj.$loaded();
+            }
+
+            function remove(templateId) {
+                let fbObj = new $firebaseObject(gameTemplatesRef.child(templateId));
+                fbObj.$remove();
+                return fbObj.$loaded();
             }
 
         }]
