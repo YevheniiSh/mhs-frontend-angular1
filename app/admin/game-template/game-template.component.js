1fb7868b5bf517fbfee705e8ee944fdf61ce70ca
ihorl <ihorlu@softwareplanet.uk.com>
2017-08-18 14:39:10 +0300

    add create empty template

--- app/admin/game-template/game-template.component.js
+++ app/admin/game-template/game-template.component.js
@@ -11,7 +11,7 @@ gameTemplate.$inject = ['$routeParams', '$location', 'gameTemplateServiceFactory
 function gameTemplate($routeParams, $location, gameTemplateService) {
     let vm = this;
 
-    vm.showTemplate = function(templateId){
+    vm.showTemplate = function (templateId) {
         $location.path(`/templates/${templateId}`);
     };
 
@@ -22,5 +22,11 @@ function gameTemplate($routeParams, $location, gameTemplateService) {
     gameTemplateService.getAll().then((val) => {
         vm.templates = val
     });
+    vm.createTemplate = function () {
+        gameTemplateService.createTemplate().then((res) => {
+            $location.path("/templates/" +  res.$id)
+            debugger
+        })
+    }
 
 }
