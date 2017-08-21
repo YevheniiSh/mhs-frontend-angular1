f9b04a208f32d2a3bcc8d198cf31a599e8cb58f4
Andrii Heiko <andriihe@softwareplanet.uk.com>
2017-08-21 12:24:17 +0300

    hide show result button for non-auth user

--- app/admin/game-results/game-results-page.component.js
+++ app/admin/game-results/game-results-page.component.js
@@ -1,52 +1,66 @@
 angular.module('gameResultsPage')
     .component('gameResultsPage', {
         templateUrl: 'admin/game-results/game-results-page.html',
-        controller: ['ResultServiceFactory', 'GameServiceFactory', '$routeParams', '$rootScope', '$location', '$window', 'userAuthService', function (ResultService, GameService, $routeParams, $rootScope, $location, $window, auth) {
+        controller: [
+            'ResultServiceFactory',
+            'GameServiceFactory',
+            '$routeParams',
+            '$rootScope',
+            '$location',
+            '$window',
+            'userAuthService',
+            function (ResultService, GameService, $routeParams, $rootScope, $location, $window, auth) {
 
-            let vm = this;
-            let gameId = $routeParams.gameId;
-            vm.isGameCurrent = true;
+                let vm = this;
+                let gameId = $routeParams.gameId;
+                vm.isGameCurrent = true;
 
-            vm.$onInit = onInit;
+                vm.$onInit = onInit;
 
-            vm.teamResults = function () {
-                $window.open($window.location.origin + `/#!/games/${gameId}/results-presentation`, ``, `width=${screen.availWidth},height=${screen.availHeight}`);
-            };
+                vm.teamResults = function () {
+                    $window.open($window.location.origin + `/#!/games/${gameId}/results-presentation`, ``, `width=${screen.availWidth},height=${screen.availHeight}`);
+                };
 
-            function onInit() {
-                vm.gameId = $routeParams.gameId;
+                function onInit() {
+                    vm.gameId = $routeParams.gameId;
 
-                GameService.getGameStatus(this.gameId).then(status => {
-                    if (status === "current")
-                        GameService.getDate(status, this.gameId).then(v => this.date = new Date(v.$value).toLocaleDateString());
-                    if (status === "finished")
-                        GameService.getDate(status, this.gameId).then(v => this.date = new Date(v.$value).toLocaleDateString())
-                });
-
-                ResultService.getParsedResults(this.gameId)
-                    .then((result) => {
-                        vm.results = result;
-                        console.log(vm.results);
+                    GameService.getGameStatus(this.gameId).then(status => {
+                        if (status === "current")
+                            GameService.getDate(status, this.gameId).then(v => this.date = new Date(v.$value).toLocaleDateString());
+                        if (status === "finished")
+                            GameService.getDate(status, this.gameId).then(v => this.date = new Date(v.$value).toLocaleDateString())
                     });
-                GameService.getGameStatus(gameId).then(status => {
-                    (status === "finished") ?
-                        vm.shareButton = true : vm.shareButton = false;
+
+                    ResultService.getParsedResults(this.gameId)
+                        .then((result) => {
+                            vm.results = result;
+                            console.log(vm.results);
+                        });
+                    GameService.getGameStatus(gameId).then(status => {
+                        (status === "finished") ?
+                            vm.shareButton = true : vm.shareButton = false;
+                    });
+                }
+
+                vm.shareURL = $location.absUrl();
+
+
+                vm.onBack = function () {
+                    auth.currentUser()
+                        .then(() => {
+                            $location.path(`/games/${gameId}/rounds`);
+                        })
+                        .catch(() => {
+                            $location.path(`/games`);
+                        })
+                    ;
+                }
+
+                auth.currentUser().then((res) => {
+                    vm.auth = true;
+                }).catch((err) => {
+                    vm.auth = false;
                 });
-            }
-
-            vm.shareURL = $location.absUrl();
-
-
-            vm.onBack = function () {
-                auth.currentUser()
-                    .then(() => {
-                        $location.path(`/games/${gameId}/rounds`);
-                    })
-                    .catch(() => {
-                        $location.path(`/games`);
-                    })
-                ;
-            }
-        }]
+            }]
 
     });
