a0093b0d6c483522b3c6777438624a25b95e4af1
yevheniish <yevheniishc@softwareplanet.uk.com>
2017-08-25 11:46:10 +0300

    remove save latency while team results is editing

--- app/admin/team-results/team-results.component.js
+++ app/admin/team-results/team-results.component.js
@@ -1,114 +1,118 @@
-angular.module('teamResults')
-    .component('teamResults', {
-        templateUrl: 'admin/team-results/team-results.html',
-        controller: ['userAuthService', 'GameServiceFactory', 'ResultServiceFactory', 'RoundStatusService', 'TeamServiceFactory', '$routeParams', '$rootScope', '$location', '$window',
-            function (userAuthService, GameService, ResultService, RoundService, TeamService, $routeParams, $rootScope, $location, $window) {
-                let vm = this;
-                this.$onInit = onInit;
+(function () {
+    angular.module('teamResults')
+        .component('teamResults', {
+            templateUrl: 'admin/team-results/team-results.html',
+            controller: TeamResultsController
+        });
 
-                function onInit() {
+    TeamResultsController.$inject = ['userAuthService', 'GameServiceFactory', 'ResultServiceFactory', 'TeamServiceFactory', '$routeParams', '$window',];
+
+    function TeamResultsController(userAuthService, GameService, ResultService, TeamService, $routeParams, $window) {
+        let vm = this;
+        this.$onInit = onInit;
+
+        function onInit() {
+            vm.gameStatus = true;
+
+            vm.gameId = $routeParams.gameId;
+            vm.teamId = $routeParams.teamId;
+
+            vm.auth = false;
+            userAuthService.currentUser().then(() => {
+                vm.auth = true;
+            }).catch(() => {
+                vm.auth = false;
+            });
+
+            GameService.getGameStatus(this.gameId).then(status => {
+                if (status === "current") {
+                    vm.state = status;
+                    vm.gameStatus = false;
+                    GameService.getDate(status, this.gameId).then(v => this.date = new Date(v.$value).toLocaleDateString())
+                }
+                if (status === "finished") {
+                    vm.state = status;
                     vm.gameStatus = true;
-
-                    vm.gameId = $routeParams.gameId;
-                    vm.teamId = $routeParams.teamId;
-
-                    vm.auth = false;
-                    userAuthService.currentUser().then((res) => {
-                        vm.auth = true;
-                    }).catch((err) => {
-                        vm.auth = false;
-                    });
-
-                    GameService.getGameStatus(this.gameId).then(status => {
-                        if (status === "current") {
-                            vm.state = status;
-                            vm.gameStatus = false;
-                            GameService.getDate(status, this.gameId).then(v => this.date = new Date(v.$value).toLocaleDateString())
-                        }
-                        if (status === "finished") {
-                            vm.state = status;
-                            vm.gameStatus = true;
-                            GameService.getDate(status, this.gameId).then(v => this.date = new Date(v.$value).toLocaleDateString())
-                        }
-                    });
-
-                    vm.getResults();
+                    GameService.getDate(status, this.gameId).then(v => this.date = new Date(v.$value).toLocaleDateString())
                 }
+            });
 
-                vm.showGameResults = function () {
-                    $window.history.back();
-                };
-                vm.url = $routeParams.gameId;
+            vm.getResults();
+        }
 
-                vm.getResults = function () {
-                    ResultService.filter({by: 'teamId', val: $routeParams.teamId}, $routeParams.gameId)
-                        .then(teamResults=>{
-                            return ResultService.parseTeamResult(teamResults, vm.gameId)
-                        })
-                        .then((res) => {
-                            vm.teamTotal = 0;
+        vm.showGameResults = function () {
+            $window.history.back();
+        };
+        vm.url = $routeParams.gameId;
 
-                            vm.roundsResult = res
-                            angular.forEach(res, (round) => {
-                                if (round.total) {
-                                    vm.teamTotal += parseFloat(round.total);
-                                }
-                            });
-                        });
+        vm.getResults = function () {
+            ResultService.filter({by: 'teamId', val: $routeParams.teamId}, $routeParams.gameId)
+                .then(teamResults => {
+                    return ResultService.parseTeamResult(teamResults, vm.gameId)
+                })
+                .then((res) => {
+                    vm.teamTotal = 0;
 
-                    TeamService.getById($routeParams.teamId)
-                        .then(team => {
-                            vm.teamName = team.name;
-                        });
-                };
+                    vm.roundsResult = res;
+                    angular.forEach(res, (round) => {
+                        if (round.total) {
+                            vm.teamTotal += parseFloat(round.total);
+                        }
+                    });
+                });
 
-                vm.setTeamResult = function (round, quiz) {
-                    let score = parseFloat(quiz.score);
-                    let quizNum = parseFloat(quiz.quizNum);
-                    let roundNum = parseFloat(round.roundNum);
+            TeamService.getById($routeParams.teamId)
+                .then(team => {
+                    vm.teamName = team.name;
+                });
+        };
 
-                    let result = {
-                        quiz: quizNum,
-                        round: roundNum,
-                        score: score,
-                        teamId: vm.teamId
-                    };
+        vm.setTeamResult = function (round, quiz) {
+            if (quiz.score == undefined || null) return;
+            let score = parseFloat(quiz.score);
+            let quizNum = parseFloat(quiz.quizNum);
+            let roundNum = parseFloat(round.roundNum);
 
-                    ResultService.saveResult(vm.state, result, vm.gameId);
-                    vm.getResults();
-                };
+            let result = {
+                quiz: quizNum,
+                round: roundNum,
+                score: score,
+                teamId: vm.teamId
+            };
 
-                this.totalColor = function (round) {
-                    let total = parseFloat(round.total);
-                    if (total === 0) {
-                        return 'silver-total'
-                    } else if (total > 0) {
-                        return 'text-success'
-                    } else {
-                        return 'text-danger'
-                    }
-                };
-                this.quizColor = function (quiz) {
-                    let score = parseFloat(quiz.score);
+            ResultService.saveResult(vm.state, result, vm.gameId);
+        };
 
-                    if (score === 0) {
-                        return 'btn-silver';
-                    } else if (score > 0) {
-                        return 'btn-success';
-                    } else {
-                        return 'btn-danger';
-                    }
-                };
+        vm.totalColor = function (round) {
+            let total = parseFloat(round.total);
+            if (total === 0) {
+                return 'silver-total'
+            } else if (total > 0) {
+                return 'text-success'
+            } else {
+                return 'text-danger'
+            }
+        };
+        vm.quizColor = function (quiz) {
+            let score = parseFloat(quiz.score);
 
-                this.editResults = function () {
-                    this.gameStatus = false;
-                };
+            if (score === 0) {
+                return 'btn-silver';
+            } else if (score > 0) {
+                return 'btn-success';
+            } else {
+                return 'btn-danger';
+            }
+        };
 
-                this.blockEditing = function () {
-                    this.gameStatus = true;
-                    ResultService.setGameWinner(this.state, vm.gameId);
-                    ResultService.setTeamsResults(vm.gameId);
-                }
+        vm.editResults = function () {
+            this.gameStatus = false;
+        };
 
-            }]
-    });
+        vm.blockEditing = function () {
+            this.gameStatus = true;
+            ResultService.setGameWinner(this.state, vm.gameId);
+            ResultService.setTeamsResults(vm.gameId);
+        }
+    }
+})();
