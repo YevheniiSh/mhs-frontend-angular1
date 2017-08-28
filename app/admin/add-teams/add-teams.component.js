5f3ec9fcfce7abf43ed09b5ec00e69a605822068
OleksiiLy <oleksiily@softwareplanetgroup.com>
2017-08-28 12:50:25 +0300

    add editable team size

--- app/admin/add-teams/add-teams.component.js
+++ app/admin/add-teams/add-teams.component.js
@@ -80,6 +80,10 @@ angular.module('addTeams')
                     teamService.removeGameFromTeam(team.$id, vm.gameId);
                     gameRequestService.setUnconfirmedStatus(vm.gameId, team.requestId);
                 }
+
+                vm.updateTeamSize = function (teamId, numberOfPlayers) {
+                    openGameService.updateTeamSize(vm.gameId, teamId, numberOfPlayers);
+                }
             }]
 
     })
