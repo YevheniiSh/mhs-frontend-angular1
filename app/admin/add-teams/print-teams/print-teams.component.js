342a6442baa61043e5386e9dd4c5cc1879e17d1e
ihorl <ihorlu@softwareplanet.uk.com>
2017-08-29 15:07:55 +0300

    add print teams

--- app/admin/add-teams/print-teams/print-teams.component.js
+++ app/admin/add-teams/print-teams/print-teams.component.js
@@ -2,15 +2,22 @@ angular.module('addTeams')
     .component('printTeams', {
         templateUrl: 'admin/add-teams/print-teams/print-teams.html',
         css: 'admin/add-teams/print-teams/print-teams.css',
-        bindings: {
-            teams: '='
-        },
-        controller: ['teamRequestService','gameRequestServiceFactory',
+        controller: ['OpenGameServiceFactory', '$routeParams', '$timeout', '$window',
 
-            function () {
+            function (openGameService, $routeParams, $timeout, $window) {
                 let vm = this;
+                let gameId = $routeParams.gameId;
 
+                openGameService.getTeams(gameId)
+                    .then(res => {
+                        vm.teams = res;
 
+                    })
+                    .then(() => {
+                        $timeout(function () {
+                            window.print();
+                            window.close();
+                        })
+                    })
             }]
-
     });
