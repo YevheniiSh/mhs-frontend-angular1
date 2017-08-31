cd23a677a4c0b768cd7a26ac6bd6d20c6260577a
Andrii Heiko <andriihe@softwareplanet.uk.com>
2017-08-31 10:28:47 +0300

    add save method to season service

--- app/services/season-service/season-service.service.js
+++ app/services/season-service/season-service.service.js
@@ -1,17 +1,34 @@
 angular.module('seasonService')
     .factory('seasonService', ['$firebaseArray', '$firebaseObject', 'firebaseDataService',
         function ($firebaseArray, $firebaseObject, firebaseDataService) {
+
+            let seasonRef = firebaseDataService.seasons;
+
             return {
+                save: save,
                 getSeasonsNames: getSeasonsNames
             };
 
-            function getSeasonsNames() {
-                return new $firebaseArray(firebaseDataService.seasons)
+            function save(season) {
+                let obj = new $firebaseObject(seasonRef.push());
+                obj.$value = season;
+                obj.$save();
+                return obj
                     .$loaded()
                     .then((res) => {
-                        let seasons=[];
-                        res.forEach(season=>{
-                            seasons.push({id:season.$id,name:season.name})
+                        return res.$id;
+                    }, (err) => {
+                        return err;
+                    });
+            }
+
+            function getSeasonsNames() {
+                return new $firebaseArray(seasonRef)
+                    .$loaded()
+                    .then((res) => {
+                        let seasons = [];
+                        res.forEach(season => {
+                            seasons.push({id: season.$id, name: season.name})
                         })
                         return seasons;
                     }, (err) => {
