724e6dc3bbceba19f7f900dc04bc4444f66b61ca
AntonTry <antontr@interlink-ua.com>
2017-09-06 11:48:20 +0300

    Add 3 state radio switch layout

--- app/admin/round-type/hints/hints-round.component.js
+++ app/admin/round-type/hints/hints-round.component.js
@@ -2,7 +2,11 @@ angular.module('resultSetup')
     .component('hintsRound', {
         templateUrl: 'admin/round-type/hints/hints-round.html',
         css: 'admin/round-type/hints/hints-round.css',
-        controller: hintsRoundController
+        controller: hintsRoundController,
+        bindings: {
+            results: "=",
+            saveResult: "&"
+        }
     });
 
 hintsRoundController.$inject = [
@@ -15,5 +19,34 @@ hintsRoundController.$inject = [
 function hintsRoundController(resultSetupService, $routeParams, $location, $scope) {
     let vm = this;
 
-    vm.isManualInput = false;
+    vm.$onInit = onInit;
+
+    function onInit() {
+    //     getRound()
+    //         .then(getQuizWeight);
+    //     initPreviousQuizResults();
+    // }
+    //
+    // function getRound() {
+    //     return GameServiceFactory.getRoundByGameAndId($routeParams.gameId, $routeParams.roundNumber);
+    // }
+    //
+    // function getQuizWeight(round) {
+    //     vm.weight = round.roundType.start + (round.roundType.step * ($routeParams.quizNumber - 1));
+    // }
+    //
+    // function initPreviousQuizResults() {
+    //     ResultServiceFactory.getByRoundAndQuiz(
+    //         $routeParams.roundNumber,
+    //         $routeParams.quizNumber-1,
+    //         $routeParams.gameId
+    //     )
+    //         .then(results=>{
+    //             vm.previousQuizResults = results;
+    //         })
+    // }
+    //
+    // vm.inDisabled = function(teamId) {
+    //
+    }
 }
