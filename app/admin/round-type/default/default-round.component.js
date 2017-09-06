292033afe778ad30b3c97efdbaa6b5a1124c9028
yevheniish <yevheniishc@softwareplanet.uk.com>
2017-09-06 17:21:51 +0300

    optimise save ang get data

--- app/admin/round-type/default/default-round.component.js
+++ app/admin/round-type/default/default-round.component.js
@@ -20,35 +20,28 @@
         function onInit() {
             vm.selectedQuiz = $routeParams.quizNumber;
 
-            getQuizResults()
-                .then(getWeightOfResponse);
-
             initDefaultNumberOfCorrectAnswers();
         }
 
         function initDefaultNumberOfCorrectAnswers() {
-            $scope.$watch(() => {
+            let unregister = $scope.$watch(() => {
                 return vm.results;
             }, (results) => {
                 if (results !== undefined) {
+                    unregister();
                     results.forEach((result) => {
+                        getWeightOfResponse(result);
                         result.numberOfCorrectAnswers === undefined ? result.numberOfCorrectAnswers = 0 : result.numberOfCorrectAnswers
                     });
                 }
             })
         }
 
-        function getWeightOfResponse(quizzes) {
-            quizzes.forEach((quiz) => {
-                if (quiz.weightOfResponse !== undefined) {
-                    vm.isManualInput = true;
-                }
-                vm.weightOfResponse = quiz.weightOfResponse;
-            })
-        }
-
-        function getQuizResults() {
-            return resultSetupService.getQuizResults($routeParams.roundNumber, vm.selectedQuiz, $routeParams.gameId);
+        function getWeightOfResponse(result) {
+            if (result.weightOfResponse !== undefined) {
+                vm.isManualInput = true;
+            }
+            vm.weightOfResponse = result.weightOfResponse;
         }
 
         function calculateScore(result) {
@@ -74,10 +67,14 @@
         };
 
         vm.recalculateScore = function () {
-            vm.results.forEach((result) => {
+            for (let result of vm.results) {
                 result.score = calculateScore(result);
                 save(result);
-            })
+            }
+        };
+
+        vm.selectAllContent = function ($event) {
+            $event.target.select();
         };
     }
 })();
