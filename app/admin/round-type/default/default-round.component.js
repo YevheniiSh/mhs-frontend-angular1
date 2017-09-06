186592f240d9175ddb129085035418419c2eca00
yevheniish <yevheniishc@softwareplanet.uk.com>
2017-09-06 08:46:27 +0300

    add weight of response skeleton

--- app/admin/round-type/default/default-round.component.js
+++ app/admin/round-type/default/default-round.component.js
@@ -1,30 +1,45 @@
-angular.module('resultSetup')
-    .component('defaultRound', {
-        templateUrl: 'admin/round-type/default/default-round.html',
-        css: 'admin/round-type/default/default-round.css',
-        controller: defaultRoundController,
-        bindings: {
-            results: '=',
-            isManualInput: '=',
-            saveResult: '&'
+(function () {
+    angular.module('resultSetup')
+        .component('defaultRound', {
+            templateUrl: 'admin/round-type/default/default-round.html',
+            css: 'admin/round-type/default/default-round.css',
+            controller: DefaultRoundController,
+            bindings: {
+                results: '=',
+                isManualInput: '=',
+                onSave: '&'
+            }
+        });
+
+    DefaultRoundController.$inject = [];
+
+    function DefaultRoundController() {
+        let vm = this;
+
+        vm.$onInit = onInit;
+
+        function onInit() {
+            vm.weightOfResponse = 1;
         }
-    });
 
-defaultRoundController.$inject = [
-    'resultSetupService',
-    '$routeParams',
-    '$location',
-];
+        function calculateScore(result) {
+            return Math.floor(result.numberOfCorrectAnswers * vm.weightOfResponse * 100) / 100
+        }
 
-function defaultRoundController(resultSetupService, $routeParams, $location) {
-    let vm = this;
+        vm.saveResult = function (result) {
+            if (vm.isManualInput) {
+                result.score = calculateScore(result);
+                vm.onSave({result: result});
+                return;
+            }
+            vm.onSave({result: result});
+        };
 
-    vm.isManualInput = false;
-
-
-    vm.$onInit = onInit;
-
-    function onInit() {
-
+        vm.recalculateScore = function () {
+            vm.results.forEach((result) => {
+                result.score = calculateScore(result);
+                vm.onSave({result: result});
+            })
+        };
     }
-}
+})();
