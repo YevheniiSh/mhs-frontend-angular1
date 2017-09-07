1560fa8c03b4d5bd583ff919d87bb06e20d3a6c0
ihorl <ihorlu@softwareplanet.uk.com>
2017-09-07 16:22:05 +0300

    add default html to default-result component

--- app/admin/team-result-types/default-result/default-result.component.js
+++ app/admin/team-result-types/default-result/default-result.component.js
@@ -2,6 +2,11 @@ angular.module('teamResults')
     .component('defaultResult', {
         templateUrl: 'admin/team-result-types/default-result/default-result.html',
         css: 'admin/team-result-types/default-result/default-result.css',
+        bindings: {
+            round: '=',
+            resultDisabled: '<',
+            saveResult: '&'
+        },
         controller: defaultResultController
     });
 
