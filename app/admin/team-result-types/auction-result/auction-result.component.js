ca76ae7882ed4f283650ac30a5fe58b93548e364
yevheniish <yevheniishc@softwareplanet.uk.com>
2017-09-08 14:32:45 +0300

    add auction edit result feature

--- app/admin/team-result-types/auction-result/auction-result.component.js
+++ app/admin/team-result-types/auction-result/auction-result.component.js
@@ -3,7 +3,7 @@ angular.module('teamResults')
         templateUrl: 'admin/team-result-types/auction-result/auction-result.html',
         css: 'admin/team-result-types/auction-result/auction-result.css',
         controller: AuctionResultController,
-        bindings:{
+        bindings: {
             round: "=",
             resultDisabled: "=",
             saveResult: "&"
@@ -12,16 +12,13 @@ angular.module('teamResults')
 
 AuctionResultController.$inject = [];
 
-function AuctionResultController(){
+function AuctionResultController() {
 
     let vm = this;
 
-    vm.editResult = false;
-
     vm.getClass = function (quiz) {
         let score = +quiz.score;
-        console.log(score);
-        switch (score){
+        switch (score) {
             case score > 0:
                 return "btn-success";
             case score < 0:
@@ -29,45 +26,28 @@ function AuctionResultController(){
             default:
                 return "btn-silver"
         }
+    };
+
+    vm.initResults = function (result) {
+        result.rate = Math.abs(result.score);
+        setResultStatus(result)
+    };
+
+    vm.onSave = function (roundNumber, result) {
+        result.score = calculateScore(result);
+        vm.saveResult({roundNum: roundNumber, quiz: result});
+    };
+
+    function calculateScore(result) {
+        return result.rate * result.status;
     }
-    //
-    // vm.changeQuizState = function () {
-    //     if (!vm.editResult){
-    //         vm.quiz.score = -vm.quiz.score
-    //     }
-    // }
-    //
-    // $scope.$watch(() => {
-    //     return vm.results;
-    // }, (res) => {
-    //     if (res !== undefined && res[0].rate === undefined)
-    //         initResults(res);
-    // });
-    //
-    // vm.onSave = function (result) {
-    //     result.score = result.rate * result.status;
-    //     vm.saveResult({result: result})
-    // };
-    //
-    // function initResults(res) {
-    //     for (let result of res) {
-    //         if (result.score !== undefined) {
-    //             result.rate = Math.abs(result.score);
-    //
-    //             setResultStatus(result);
-    //         }
-    //         else {
-    //             result.rate = 0;
-    //             result.status = -1
-    //         }
-    //     }
-    // }
-    //
-    // function setResultStatus(result) {
-    //     if (result.score <= 0) {
-    //         result.status = -1
-    //     } else
-    //         result.status = 1
-    // }
+
+    function setResultStatus(result) {
+        if (result.score <= 0) {
+            result.status = -1;
+        } else {
+            result.status = 1;
+        }
+    }
 
 }
