1e8d536cf370dbf466b3e2ec3aa239e16f926b09
ihorl <ihorlu@softwareplanet.uk.com>
2017-09-08 14:54:59 +0300

    add edit result with weight

--- app/admin/team-result-types/default-result/default-result.component.js
+++ app/admin/team-result-types/default-result/default-result.component.js
@@ -12,6 +12,24 @@ angular.module('teamResults')
 
 defaultResultController.$inject = [];
 
-function defaultResultController(){
+function defaultResultController() {
+    let vm = this;
 
+
+    vm.$onInit = onInit;
+
+    function onInit() {
+        for (let quiz of vm.round.quizzes) {
+            if (quiz.hasOwnProperty("weightOfResponse")) {
+
+                quiz.countAnswer=+((quiz.score / quiz.weightOfResponse).toFixed(1));
+
+            }
+        }
+    }
+
+    vm.setScore = function (roundNum, quiz) {
+        quiz.score = +((quiz.weightOfResponse * quiz.countAnswer).toFixed(1));
+        vm.saveResult({roundNum: roundNum, quiz: quiz})
+    }
 }
