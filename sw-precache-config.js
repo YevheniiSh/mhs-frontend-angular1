fb57471ccd5327962957938db2b4af13a3271a66
Andrii Heiko <andriihe@softwareplanet.uk.com>
2017-09-28 12:24:25 +0300

    add cache large files

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,11 +3,18 @@ module.exports = {
     'dist/**.html',
     'dist/**.js',
     'dist/**.css',
+    'dist/**.eot',
+    'dist/**.ico',
+    'dist/**.svg',
+    'dist/**.woff2',
+    'dist/**.ttf',
+    'dist/**.woff',
     'dist/app/**',
     'dist/assets/images/*',
     'dist/assets/icons/*'
   ],
   root: 'dist',
   stripPrefix: 'dist/',
-  navigateFallback: '/index.html'
+  navigateFallback: '/index.html',
+  maximumFileSizeToCacheInBytes:8388608
 };
