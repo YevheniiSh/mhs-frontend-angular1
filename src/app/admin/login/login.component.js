2c6d0223a58a3acd016439b78e1652d7b8e2a052
ihorl <ihorlu@softwareplanet.uk.com>
2017-10-02 10:29:54 +0300

    refactor login component

--- src/app/admin/login/login.component.js
+++ src/app/admin/login/login.component.js
@@ -1,28 +1,28 @@
-angular.module('login')
+(function () {
+  angular.module('login')
     .component('login', {
       templateUrl: "app/admin/login/login.html",
       css: 'app/admin/login/login.css',
-        controller: [
-            'userAuthService',
-          '$window',
-            '$rootScope',
-          'login',
-          function (auth, $window, $rootScope, loginService) {
-                this.login = function () {
-                    auth.signInWithEmailAndPassword(this.email, this.password)
-                        .then((user) => {
-                            $rootScope.currentUser = user.email;
-                          $window.history.back();
-                        })
-                        .catch((err) => {
-                            if (err === 'rulesError'){
-                              this.errMessage = "INVALID_RULES_MESSAGE";
-                              return;
-                            }
-                            this.errMessage = "INVALID_EMAIL_MESSAGE";
-                        });
-                  loginService.login(this.email, this.password);
-                };
-
-            }]
+      controller: loginController
     });
+
+  loginController.$inject = ['userAuthService', '$window', '$rootScope', 'login'];
+
+  function loginController(auth, $window, $rootScope, loginService) {
+    this.login = function () {
+      auth.signInWithEmailAndPassword(this.email, this.password)
+        .then((user) => {
+          $rootScope.currentUser = user.email;
+          $window.history.back();
+        })
+        .catch((err) => {
+          if (err === 'rulesError') {
+            this.errMessage = "INVALID_RULES_MESSAGE";
+            return;
+          }
+          this.errMessage = "INVALID_EMAIL_MESSAGE";
+        });
+      loginService.login(this.email, this.password);
+    };
+  }
+})();
