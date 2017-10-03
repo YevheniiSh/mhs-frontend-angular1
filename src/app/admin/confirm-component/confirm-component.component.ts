497aba0d85bd258761987af81001d2bb586d6eba
bohdanh <bohdanhu@softwareplanet.uk.com>
2017-10-03 14:12:55 +0300

    Add ng2-bootstrap modal library and create modal component

---
+++ src/app/admin/confirm-component/confirm-component.component.ts
@@ -0,0 +1,33 @@
+import {Component} from '@angular/core';
+import {DialogComponent, DialogService} from "ng2-bootstrap-modal";
+
+export interface ConfirmModel {
+  title: string;
+  message: string;
+}
+
+@Component({
+  selector: 'app-confirm-component',
+  templateUrl: './confirm-component.component.html'
+})
+export class ConfirmComponentComponent extends DialogComponent<ConfirmModel, boolean> implements ConfirmModel {
+  title: string;
+  message: string;
+
+  constructor(dialogService: DialogService) {
+    super(dialogService);
+  }
+
+  confirm() {
+    // on click on confirm button we set dialog result as true,
+    // ten we can get dialog result from caller code
+    this.result = true;
+    this.close();
+  }
+
+  cancel() {
+    this.result = false;
+    this.close();
+  }
+
+}
