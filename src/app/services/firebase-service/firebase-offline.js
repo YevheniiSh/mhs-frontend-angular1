77f5714a7527ad3ffefb8fc521f4999dc48d7093
OleksiiLy <oleksiily@softwareplanetgroup.com>
2017-10-05 14:50:12 +0300

    add subscribe to data for offline

--- src/app/services/firebase-service/firebase-offline.js
+++ src/app/services/firebase-service/firebase-offline.js
@@ -5,17 +5,17 @@ import 'rxjs/add/observable/fromPromise';
 import { IntervalObservable } from 'rxjs/observable/IntervalObservable';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/distinctUntilChanged';
-const isOnline = require('is-online');
 
 angular
   .module('firebaseDataService')
-  .factory('firebaseOfflineService', firebaseOfflineService);
+  .factory('firebaseOfflineService',firebaseOfflineService);
 
 function firebaseOfflineService() {
 
+  initData();
+
   return {
-    enableOffline: enableOffline,
-    isConnected: isConnected
+    enableOffline: enableOffline
   };
 
   function enableOffline() {
@@ -34,11 +34,15 @@ function firebaseOfflineService() {
     return currentGamesRef.update(game);
   }
 
-  function isConnected() {
-    return IntervalObservable
-      .create(2000)
-      .switchMap((test) => Observable.fromPromise(isOnline()))
-      .distinctUntilChanged();
+  function initData(){
+    let ref = firebase.database().ref();
+    ref.child('games/current/').on('value', () => {});
+    ref.child('games/open/').on('value', () => {});
+    ref.child('teams/').on('value', () => {});
+    ref.child('seasons/').on('value', () => {});
+    ref.child('gameTemplates/').on('value', () => {});
+    ref.child('roundTypes/').on('value', () => {});
+    ref.child('teamsRequests/').on('value', () => {});
   }
 }
 
