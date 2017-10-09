4d322d1d4a3108162d2d873b44a4b94c9e7f357a
OleksiiLy <oleksiily@softwareplanetgroup.com>
2017-10-09 12:19:05 +0300

    replace onableOffline to initData method

--- src/app/app.js
+++ src/app/app.js
@@ -28,5 +28,5 @@ angular.module('mhs', [
       $routeProvider.otherwise({redirectTo: '/games'});
     }])
   .run(['firebaseOfflineService', (firebaseOfflineService) => {
-    firebaseOfflineService.enableOffline();
+    firebaseOfflineService.initData();
   }]);
