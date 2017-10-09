4700c0c81a580e8c174c1873c4b4eb1d5ee6c535
bohdanh <bohdanhu@softwareplanet.uk.com>
2017-10-09 15:47:48 +0300

    Fix review bugs

--- src/app/admin/edit-game/edit-game.component.ts
+++ src/app/admin/edit-game/edit-game.component.ts
@@ -38,59 +38,68 @@ export class EditGameComponent implements OnInit {
   }
 
   setupGameInfo() {
-    this.openGameService.getDate(this.gameId).then((res) => {
-      this.currentGameDate = new Date(res);
-    });
+    this.openGameService.getDate(this.gameId)
+      .then((res) => {
+        this.currentGameDate = new Date(res);
+      });
 
-    this.openGameService.getTime(this.gameId).then((res) => {
-      this.currentGameTime = new Date(res);
-    });
+    this.openGameService.getTime(this.gameId)
+      .then((res) => {
+        this.currentGameTime = new Date(res);
+      });
 
-    this.openGameService.getLocation(this.gameId).then((res) => {
-      this.location = res;
-    });
+    this.openGameService.getLocation(this.gameId)
+      .then((res) => {
+        this.location = res;
+      });
   }
 
 
   setGameLocation() {
-    this.openGameService.changeLocation(this.gameId, this.location).then(() => {
-      this.notificationService.showSuccess('SAVE_LOCATION_MESSAGE');
-    });
+    this.openGameService.changeLocation(this.gameId, this.location)
+      .then(this.showSuccess('SAVE_LOCATION_MESSAGE'));
   }
 
   setGameDate(gameDate) {
     this.gameDate = gameDate;
-    this.openGameService.changeDate(this.gameId, this.gameDate).then(() => {
-      this.notificationService.showSuccess('SAVE_DATE_MESSAGE');
-    });
+    this.openGameService.changeDate(this.gameId, this.gameDate)
+      .then(this.showSuccess('SAVE_DATE_MESSAGE'));
   }
 
   setGameTime(gameTime) {
     this.gameTime = gameTime;
-    this.openGameService.changeTime(this.gameId, this.gameTime).then(() => {
-      this.notificationService.showSuccess('SAVE_TIME_MESSAGE');
-    });
+    this.openGameService.changeTime(this.gameId, this.gameTime)
+      .then(this.showSuccess('SAVE_TIME_MESSAGE'));
+
   }
 
   setIsSeasonGame(isSeasonGame) {
     this.isSeasonGame = isSeasonGame;
     if (this.isSeasonGame) {
       let seasonInGame = { id: this.season.$id, name: this.season.name };
-      this.openGameService.changeSeason(this.gameId, seasonInGame).then(() => {
-        this.notificationService.showSuccess('SAVE_SEASON_MESSAGE');
-        this.seasonService.addGameToSeason(this.season.$id, this.gameId)
-      });
+      this.openGameService.changeSeason(this.gameId, seasonInGame)
+        .then(() => {
+          return this.seasonService.addGameToSeason(this.season.$id, this.gameId)
+        })
+        .then(this.showSuccess('SAVE_SEASON_MESSAGE'));
     }
     else {
-      this.openGameService.deleteSeason(this.gameId).then(() => {
-        this.notificationService.showSuccess('SAVE_SEASON_MESSAGE');
-        this.seasonService.deleteGameFromSeason(this.season.$id, this.gameId);
-      });
+      this.openGameService.deleteSeason(this.gameId)
+        .then(() => {
+          return this.seasonService.deleteGameFromSeason(this.season.$id, this.gameId);
+        })
+        .then(this.showSuccess('SAVE_SEASON_MESSAGE'));
     }
   }
 
   setSeason(season) {
     this.season = season;
+  }
+
+  showSuccess(message) {
+    return () => {
+      this.notificationService.showSuccess(message);
+    }
   }
 
 }
