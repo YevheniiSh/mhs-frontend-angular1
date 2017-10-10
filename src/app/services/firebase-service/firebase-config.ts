c5b6bab87c808620aeb974426a31fed119171a77
ihorl <ihorlu@softwareplanet.uk.com>
2017-10-10 10:46:33 +0300

    fix configs

--- src/app/services/firebase-service/firebase-config.ts
+++ src/app/services/firebase-service/firebase-config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDBaZIQ4HQy4_v4lOjFAxDNvsA47uJGAXM',
-  authDomain: 'test-41ed5.firebaseapp.com',
-  databaseURL: 'https://test-41ed5.firebaseio.com',
-  projectId: 'test-41ed5',
-  storageBucket: 'test-41ed5.appspot.com',
-  messagingSenderId: '611265693951'
+  apiKey: 'AIzaSyDlmBFGxhXo5KdhA0FbBYdOs99j8UvDN6I',
+  authDomain: 'test-99525.firebaseapp.com',
+  databaseURL: 'https://test-99525.firebaseio.com',
+  projectId: 'test-99525',
+  storageBucket: 'test-99525.appspot.com',
+  messagingSenderId: '734509405972'
 };
