ef9c7bf62f78f13551a984fe5427516d5b7b737f
yevheniish <yevheniishc@softwareplanet.uk.com>
2017-10-11 13:44:18 +0300

    remove unused provider

--- src/app/hybrid/angular-js.provider.ts
+++ src/app/hybrid/angular-js.provider.ts
@@ -3,7 +3,6 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 const $translateToken = '$translate';
 const $routeParamsToken = '$routeParams';
 const $locationToken = '$location';
-const $cssToken = '$css';
 
 export function translateInjector(i: any) {
   return i.get($translateToken);
@@ -17,10 +16,6 @@ export function locationInjector(i: any) {
   return i.get($locationToken);
 }
 
-export function cssInjector(i: any) {
-  return i.get($cssToken);
-}
-
 @NgModule()
 export class AngularJsProvider {
   static forRoot(): ModuleWithProviders {
@@ -30,7 +25,6 @@ export class AngularJsProvider {
         { provide: $translateToken, useFactory: translateInjector, deps: ['$injector'] },
         { provide: $routeParamsToken, useFactory: routeParamsInjector, deps: ['$injector'] },
         { provide: $locationToken, useFactory: locationInjector, deps: ['$injector'] },
-        { provide: $cssToken, useFactory: cssInjector, deps: ['$injector'] },
       ]
     };
   }
