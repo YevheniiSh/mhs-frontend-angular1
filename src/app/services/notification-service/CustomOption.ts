03b7ed7a3cadd23b65ca7cce121ea8a0d16c283a
OleksiiLy <oleksiily@softwareplanetgroup.com>
2017-10-13 15:10:29 +0300

    add notifications custom options, add translations for connection start

--- src/app/services/notification-service/CustomOption.ts
+++ src/app/services/notification-service/CustomOption.ts
@@ -3,6 +3,6 @@ import { ToastOptions } from 'ng2-toastr';
 export class CustomOption extends ToastOptions {
   newestOnTop = false;
   showCloseButton = true;
-  toastLife: 2000;
+  toastLife = 2000;
   enableHTML = true;
 }
